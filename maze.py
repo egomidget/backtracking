a7ee42fc0aae4a19b00029d0341f259c55bd1166
RZicman <zicmanjo@gmail.com>
2026-07-30 13:18:57 +0100

    CHANGE dim to now represent the size of the maze

--- maze.py
+++ maze.py
@@ -5,11 +5,11 @@ from queue import Queue
 
 def create_maze(dim):
     # Create a grid filled with walls
-    maze = np.ones((dim*2+1, dim*2+1), dtype=int)
+    maze = np.ones((dim, dim), dtype=int)
 
     # Define the starting point
     x, y = (0, 0)
-    maze[2*x+1, 2*y+1] = 0
+    maze[x, y] = 0
 
     # Initialize the stack with the starting point
     stack = [(x, y)]
@@ -22,9 +22,9 @@ def create_maze(dim):
 
         for dx, dy in directions:
             nx, ny = x + dx, y + dy
-            if nx >= 0 and ny >= 0 and nx < dim and ny < dim and maze[2*nx+1, 2*ny+1] == 1:
-                maze[2*nx+1, 2*ny+1] = 0
-                maze[2*x+1+dx, 2*y+1+dy] = 0
+            if nx >= 0 and ny >= 0 and nx < dim and ny < dim and maze[nx, ny] == 1:
+                maze[nx, ny] = 0
+                maze[x+dx, y+dy] = 0
                 stack.append((nx, ny))
                 break
         else:
