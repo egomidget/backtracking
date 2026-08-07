6aa1a2dd7129b02ed9ec8cc15c903ff5c5af8248
RZRobots <robertzicman@gmail.com>
2026-08-07 12:20:01 +0100

    ADD new methods for the game class in game.py, and removed unecessary code.

--- map.py
+++ map.py
@@ -11,18 +11,23 @@ MAP =  [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 
 class Map:
-    def __init__(self, maze:list = MAP, walls:list = [7]):
+    def __init__(self, maze:list = MAP):
         self.maze = maze
-        self.walls = walls or []
         self.visited = maze
 
-    def visit(self, x:int, y:int, marker:int = 7):
+    def solver_visit(self, x:int, y:int, marker:int = 7):
         self.visited[y][x] = marker
 
-    def valid_position(self, x:int, y:int) -> bool:
+    def solver_valid_position(self, x:int, y:int) -> bool:
             try:
                 if self.visited[y][x] == 0 and x >= 0 and y >= 0:
                     return True
                 return False
             except IndexError:
                 return False
+
+    def game_check(self, x:int, y:int, num:int) -> bool:
+         if self.maze[y][x] == num:
+              return True
+         else:
+              return False
